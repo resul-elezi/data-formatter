2539c9cd657ff82a2ec5a83c9d416cb0a6957bc7
Resul Elezi <resul_elezi@protonmail.com>
2024-07-26 20:47:37 +0200

    #

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
+"use strict";
+
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
