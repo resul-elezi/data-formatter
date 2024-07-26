7b619ff1a86eec6e1cac0325e54ecf8ef3830862
Resul Elezi <resul_elezi@protonmail.com>
2024-07-26 21:18:30 +0200

    #

--- script.js
+++ script.js
@@ -13,4 +13,11 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-dateOptionsSelectElement.addEventListener("change", () => {});
+dateOptionsSelectElement.addEventListener("change", () => {
+    switch (dateOptionsSelectElement.value) {
+        case "yyyy-mm-dd":
+          currentDateParagraph.textContent = formattedDate
+            .split("-")
+    
+      }
+  });
