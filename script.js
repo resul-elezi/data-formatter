07a64bcef0ff0d4d778476b5c15e4c55d2c7e52a
Resul Elezi <resul_elezi@protonmail.com>
2024-07-26 20:49:30 +0200

    #

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
+const day = date.getDate();
